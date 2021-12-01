5c6aed789ba7e29d1e3e945d26f1b2a1539e6679
ndina <libago.ndina@gmail.com>
2021-12-01 13:04:58 -0800

    exploring the consumer function

---
+++ src/main/java/functionalinterface/_Consumer.java
@@ -0,0 +1,45 @@
+package functionalinterface;
+
+import java.util.function.Consumer;
+
+public class _Consumer {
+
+    public static void main(String[] args) {
+
+        greetings(new Customer("ndina", "90909090"));
+        greetingsConsumer.accept(new Customer("Duncan", "89999999"));
+    }
+
+
+
+    //non functional
+
+    private static void greetings(Customer customer){
+        System.out.println( "hello " + customer.customerName + " your cellphone number is " + customer.customerNumber);
+    }
+
+
+    //functional
+
+    static Consumer<Customer> greetingsConsumer =
+            customer -> System.out.println( "hello " + customer.customerName + " your number is " + customer.customerNumber);
+
+
+    static class Customer {
+        private final String customerName;
+        private final String customerNumber;
+
+        public Customer(String customerName, String customerNumber) {
+            this.customerName = customerName;
+            this.customerNumber = customerNumber;
+        }
+
+        @Override
+        public String toString() {
+            return "Customer{" +
+                    "customerName='" + customerName + '\'' +
+                    ", customerNumber='" + customerNumber + '\'' +
+                    '}';
+        }
+    }
+}
