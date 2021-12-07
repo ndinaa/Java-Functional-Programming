ce17daf7011ee1b7ea0726efdb670ccb2ef00779
ndina <libago.ndina@gmail.com>
2021-12-07 06:35:01 -0800

    starting with strems

---
+++ src/main/java/streams/Streams.java
@@ -0,0 +1,45 @@
+package streams;
+
+import java.util.List;
+
+import static streams.Streams.Gender.*;
+
+public class Streams {
+
+    public static void main(String[] args) {
+        List<Person> people = List.of(
+          new Person("humbe", MALE),
+          new Person("suzan", FEMALE),
+          new Person("Duncan", MALE),
+          new Person("Mahamood", MALE),
+          new Person("Solomon", MALE)
+
+
+        );
+
+        people.stream()
+                .
+    }
+
+    static class Person {
+        private final String name;
+        private final Gender gender;
+
+        public Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", gender=" + gender +
+                    '}';
+        }
+    }
+
+    enum Gender{
+        MALE, FEMALE
+    }
+}
